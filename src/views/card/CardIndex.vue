b3bf41b77232de4e572418bd57d0f8631183b045
Eddie <eddie.chen@cargosmart>
2021-02-26 15:50:11 +0800

    add electron pack

--- src/views/card/CardIndex.vue
+++ src/views/card/CardIndex.vue
@@ -15,7 +15,7 @@ export default {
       cardList: [
         {
           positionNum: 1,
-          name: '演示卡片1',
+          name: '演示卡片999',
           id: 'card1'
           // OtherData这个是你自己定义的属性，注意不可与componentData属性名字重复
         },
